1ac4d302089ba276464728364ca0e93001206840
smallnest <smallnest@gmail.com>
2019-08-26 19:54:41 +0800

    add etcd v3 api implementation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # libkv-etcdv3-store
-libkv etcd store by client v3 API: https://github.com/docker/libkv
+
+[libkv](https://github.com/docker/libkv) only supports ETCD v2 API event if you use etcd 3.x.
+
+This project implements a libkv store via etcd v3 API.
+
---
+++ etcdv3_test.go
@@ -0,0 +1,61 @@
+package etcdv3
+
+import (
+	"testing"
+	"time"
+
+	"github.com/docker/libkv"
+	"github.com/docker/libkv/store"
+	"github.com/docker/libkv/testutils"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	client = "localhost:2379"
+)
+
+func makeEtcdClient(t *testing.T) store.Store {
+	kv, err := New(
+		[]string{client},
+		&store.Config{
+			ConnectionTimeout: 3 * time.Second,
+			Username:          "test",
+			Password:          "very-secure",
+		},
+	)
+
+	if err != nil {
+		t.Fatalf("cannot create store: %v", err)
+	}
+
+	return kv
+}
+
+func TestRegister(t *testing.T) {
+	Register()
+
+	kv, err := libkv.NewStore(ETCDV3, []string{client}, nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, kv)
+
+	if _, ok := kv.(*EtcdV3); !ok {
+		t.Fatal("Error registering and initializing etcd")
+	}
+}
+
+func TestEtcdStore(t *testing.T) {
+	kv := makeEtcdClient(t)
+	lockKV := makeEtcdClient(t)
+	ttlKV := makeEtcdClient(t)
+
+	defer testutils.RunCleanup(t, kv)
+
+	testutils.RunTestCommon(t, kv)
+
+	testutils.RunTestAtomic(t, kv)
+	testutils.RunTestWatch(t, kv)
+	testutils.RunTestLock(t, kv)
+	testutils.RunTestLockTTL(t, kv, lockKV)
+	testutils.RunTestLockWait(t, kv, lockKV)
+	testutils.RunTestTTL(t, kv, ttlKV)
+}
